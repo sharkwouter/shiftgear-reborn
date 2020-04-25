ae67d2e0ca70068566bab5ee0f28b5d45f45f5ac
Wouter Wijsman <wwijsman@live.nl>
2020-04-25 10:21:07 +0200

    Fix level not being set

--- level.c
+++ level.c
@@ -3,6 +3,8 @@
 Level CreateLevel(SDL_Renderer* renderer) {
     Level level;
     level.player = CreatePlayer(renderer);
+
+    return level;
 }
 
 void DrawLevel(SDL_Renderer *renderer, Level *level) {
